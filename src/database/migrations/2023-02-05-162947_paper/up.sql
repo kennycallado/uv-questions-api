c844e5a361ee6cc05fbd1c28fd187802b94b867f
kennycallado <kennycallado@hotmail.com>
2023-02-05 18:25:28 +0100

    FIX Agrega ON DELETE CASCADE a las relaciones

--- src/database/migrations/2023-02-05-162947_paper/up.sql
+++ src/database/migrations/2023-02-05-162947_paper/up.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS papers (
   id SERIAL PRIMARY KEY,
   user_id SERIAL NOT NULL,
   form_id SERIAL NOT NULL,
-  CONSTRAINT fk_paper_user FOREIGN KEY (user_id) REFERENCES usuarios(id),
-  CONSTRAINT fk_paper_form FOREIGN KEY (form_id) REFERENCES forms(id)
+  CONSTRAINT fk_paper_user FOREIGN KEY (user_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+  CONSTRAINT fk_paper_form FOREIGN KEY (form_id) REFERENCES forms(id) ON DELETE CASCADE
 );
 
 INSERT INTO papers (user_id, form_id) VALUES
